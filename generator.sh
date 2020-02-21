ca521b3f159af0c383b2c3cd48ddebfd6f88bf3a
KalluruS <>
2020-02-21 12:48:30 +0530

    --- Auto-Git Commit ---

--- generator.sh
+++ generator.sh
@@ -5,4 +5,6 @@
 LIST=(`find . -type d -not -path "./.git/*" -not -path "./.git" -not -path "./.autogit/*" -not -path "./.autogit" | grep -v '^\.$' | sed -e 's|./||'`)
 
 export PS4='Select an Option>>  '
-select topic in 
+select topic in ${LIST[*]}; do 
+  
+done

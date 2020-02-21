86870217ae2d78ba5dcc91089b225ee1cf4f7af0
KalluruS <>
2020-02-21 12:50:30 +0530

    --- Auto-Git Commit ---

--- generator.sh
+++ generator.sh
@@ -4,7 +4,7 @@
 
 LIST=(`find . -type d -not -path "./.git/*" -not -path "./.git" -not -path "./.autogit/*" -not -path "./.autogit" | grep -v '^\.$' | sed -e 's|./||'`)
 
-export PS4='Select an Option>>  '
+export PS3='Select an Option>>  '
 select topic in ${LIST[*]}; do 
   TOPIC=$topic 
   break 

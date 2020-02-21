195364911027e2d088a6ffff00c189f7d2c60e8b
KalluruS <>
2020-02-21 12:46:39 +0530

    --- Auto-Git Commit ---

--- generator.sh
+++ generator.sh
@@ -2,4 +2,4 @@
 
 ## Script to generate the example directory structure.
 
-LIST=()
+LIST=(`find . -type d -not -path "./.git/*" -not -path "./.git" -not -path "./.autogit/*" -not -path "./.autogit" | grep -v ''`)

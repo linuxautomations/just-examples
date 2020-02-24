f0c7f5a57c15952965ff392c120d65fc1e6ccded
KalluruS <>
2020-02-24 14:54:42 +0100

    --- Auto-Git Commit ---

--- generator.sh
+++ generator.sh
@@ -2,7 +2,7 @@
 
 ## Script to generate the example directory structure.
 
-LIST=(`find . -type d -not -path "./.git/*" -not -path "./.git" -not -path "./.autogit/*" -not -path "./.autogit" -maxdepth 2 | grep -v '^\.$' | sed -e 's|./||'`)
+LIST=(`find . -type d -not -path "./.git/*" -not -path "./.git" -not -path "./.autogit/*" -not -path "./.autogit" -maxdepth 1 | grep -v '^\.$' | sed -e 's|./||'`)
 
 export PS3='Select an Option>>  '
 select topic in ${LIST[*]}; do 
